b40b02b006e27f707c3bf029a52d446a24674a75
LeoBang <bluke8489@gmail.com>
2022-02-27 21:07:07 +0900

    revert to merge

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -3,20 +3,6 @@ import { useEffect } from "react";
 import APIConfig from "../../api/APIConfig";
 
 const GitLawn = () => {
-  // 지우고 merge할 것.
-
-  const apiConfig = new APIConfig("leobang17");
-  const dataRefiner = apiConfig.apiDataRefiner();
-  const fetchAPI = async () => {
-    const res = await dataRefiner.getCommitHistory();
-    // console.log(dataRefiner.getStartDate());
-    console.log(res);
-  };
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return <div>GitLawn</div>;
 };
 
