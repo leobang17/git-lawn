0d7845ea5114805f443a72901c5dab752230b128
LeoBang <bluke8489@gmail.com>
2022-03-27 18:58:12 +0900

    refac: add DefaultLawn to postpone render

--- src/utils/AppState.ts
+++ src/utils/AppState.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import React, { createContext, Provider } from "react";
 import { CommitHistoryType } from "../@types";
 
 export const UsernameContext = createContext<string | null>("");
@@ -6,3 +6,13 @@ export const UsernameContext = createContext<string | null>("");
 export const CommitHistoryContext = createContext<
   CommitHistoryType | undefined
 >({} as CommitHistoryType);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const CommitHistoryProvider: React.FC<Props> = ({
+  children,
+}: Props): any => {
+  return;
+};
