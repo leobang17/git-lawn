8956679960a6f13b9c3cfca1fdd064fd09f367a8
LeoBang <bluke8489@gmail.com>
2022-03-29 21:53:48 +0900

    refac: ContributionBox - seperate styledComponent

--- src/components/atoms/contributionBox/index.tsx
+++ src/components/atoms/contributionBox/index.tsx
@@ -1,29 +1,10 @@
 import React, { useContext } from "react";
-import styled from "styled-components";
 import { ContributionBoxProps, GeneralColorProps } from "../../../@types";
 import { GeneralStyleContext } from "../../../utils/AppState";
-import { CONTRIBUTION_FONT_SIZE, MonthMapper } from "../../../utils/static";
+import { MonthMapper } from "../../../utils/static";
+import { BoldSpan, TextBox } from "./index.style";
 
-const TextBox = styled.div<{ fontColor: string; backgroundColor: string }>`
-  display: inline;
-  font-size: ${CONTRIBUTION_FONT_SIZE}px;
-  background-color: ${(props) => props.backgroundColor};
-  color: ${(props) => props.fontColor};
-  line-height: ${CONTRIBUTION_FONT_SIZE * 2.75}px;
-  padding: ${CONTRIBUTION_FONT_SIZE * 0.75}px ${CONTRIBUTION_FONT_SIZE * 1.5}px
-    ${CONTRIBUTION_FONT_SIZE * 0.75}px ${CONTRIBUTION_FONT_SIZE * 1.5}px;
-  border-radius: ${CONTRIBUTION_FONT_SIZE * 0.75}px;
-`;
-
-const BoldSpan = styled.span`
-  font-weight: bold;
-`;
-
-const ContributionBox: React.FC<ContributionBoxProps> = ({
-  date,
-  count,
-  span,
-}) => {
+const ContributionBox: React.FC<ContributionBoxProps> = ({ date, count }) => {
   const { contributionFont, contributionBackground } = useContext(
     GeneralStyleContext
   ) as GeneralColorProps;
@@ -37,7 +18,7 @@ const ContributionBox: React.FC<ContributionBoxProps> = ({
       fontColor={contributionFont}
       backgroundColor={contributionBackground}
     >
-      <BoldSpan>{count} contributions</BoldSpan> on {contributionMonth}{" "}
+      <BoldSpan>{count} contributions </BoldSpan> on{contributionMonth}{" "}
       {contributionDate}, {contributionYear}
     </TextBox>
   );
